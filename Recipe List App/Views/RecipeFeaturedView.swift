9eb1c837e16740c5c87c61e68dffc688fa944c5e
Kairat Mynbayev <kairat09@gmail.com>
2023-07-18 17:23:42 +0600

    implement tapping on recipe image

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -11,6 +11,8 @@ struct RecipeFeaturedView: View {
     
     @EnvironmentObject var model:RecipeModel
     
+    @State var isDetailViewShowing = false
+    
     var body: some View {
         
         VStack(alignment: .leading, spacing: 0) {
@@ -30,20 +32,32 @@ struct RecipeFeaturedView: View {
                         
                         // Show only featured
                         if model.recipes[index].featured {
-                            ZStack {
-                                Rectangle()
-                                    .foregroundColor(.white)
+                            
+                            Button(action: {
+                                // Show the recipe detail sheet
+                                self.isDetailViewShowing = true
                                 
-                                VStack(spacing: 0) {
-                                    Image(model.recipes[index].image)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .clipped()
-                                    Text(model.recipes[index].name)
-                                    // Here it give the right result, without repetitions
-                                        .padding(5)
+                            }, label: {
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                    
+                                    VStack(spacing: 0) {
+                                        Image(model.recipes[index].image)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .clipped()
+                                        Text(model.recipes[index].name)
+                                        // Here it give the right result, without repetitions
+                                            .padding(5)
+                                    }
                                 }
-                            }
+                            })
+                            .sheet(isPresented: $isDetailViewShowing, content: {
+                                // Show RecipeDetailView
+                                RecipeDetailView(recipe: model.recipes[index])
+                            })
+                            .buttonStyle(.plain)
                             .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
                             .cornerRadius(15)
                             .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
